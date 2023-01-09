98ba48f5965c300b9d154cff178a520fdc24ce67
ovunc <ovunc.girgin@gmail.com>
2023-01-09 22:02:54 +0300

    custom key generator added.

--- src/main/java/cl/td/cache/config/RedisConfiguration.java
+++ src/main/java/cl/td/cache/config/RedisConfiguration.java
@@ -1,19 +1,33 @@
 package cl.td.cache.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableCaching
 @Configuration
-public class RedisConfiguration {
+public class RedisConfiguration implements CachingConfigurer {
+    private String KEY_SEPERATOR = "#";
+    @Bean
+    @Override
+    public KeyGenerator keyGenerator() {
+        return (target, method, params) -> {
 
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getSimpleName());
+            sb.append(KEY_SEPERATOR);
+            sb.append(method.getName());
+            sb.append(KEY_SEPERATOR);
+            for(Object param : params){
+                sb.append(param.toString());
+                sb.append(KEY_SEPERATOR);
+            }
 
+            String str = sb.toString();
+
+            return str.substring(0, str.length() - 1);
+        };
+    }
 }
