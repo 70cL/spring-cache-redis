6f1fd2e63dde2d3fcc9a264d1c4f6e9964305990
ovunc <ovunc.girgin@gmail.com>
2023-01-12 16:37:39 +0300

    redis config added to save as json

--- src/main/java/cl/td/cache/config/RedisConfiguration.java
+++ src/main/java/cl/td/cache/config/RedisConfiguration.java
@@ -1,18 +1,31 @@
 package cl.td.cache.config;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
 
 @EnableCaching
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfiguration implements CachingConfigurer {
-    private String KEY_SEPERATOR = "#";
+    private final RedisConnectionFactory redisConnectionFactory;
+
     @Bean
     @Override
     public KeyGenerator keyGenerator() {
+        String KEY_SEPERATOR = "#";
         return (target, method, params) -> {
 
             StringBuilder sb = new StringBuilder();
@@ -29,5 +42,20 @@ public class RedisConfiguration implements CachingConfigurer {
 
             return str.substring(0, str.length() - 1);
         };
+    }
+
+    //add if you want to save as json to redis
+    @Bean
+    public CacheManager redisCacheManager() {
+        RedisSerializationContext.SerializationPair<Object> jsonSerializer =
+                RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(
+                        RedisCacheConfiguration.defaultCacheConfig()
+                                .serializeValuesWith(jsonSerializer)
+                )
+                .build();
     }
 }
